642b0dec1d2613989a41f219bc6450f79f9fb146
Brandon Jackson <brandonscottjackson@gmail.com>
2012-05-31 12:47:38 -0400

    undo address bar tracking

--- content-single.php
+++ content-single.php
@@ -47,7 +47,7 @@
 		<a class="addthis_button_tweet"></a>
 		<a class="addthis_counter addthis_pill_style"></a>
 		</div>
-		<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+		<script type="text/javascript">var addthis_config = {"data_track_addressbar":false}; </script>
 		<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fc7945601c00ae5"></script>
 		<!-- AddThis Button END -->
 
