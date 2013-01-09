0322cf1fc2906633792d2ac231bc42e7d100e82d
Brandon Jackson <brandonscottjackson@gmail.com>
2013-01-09 17:46:02 -0500

    error message added to state without stories, issue headings made consistent

--- taxonomy-state.php
+++ taxonomy-state.php
@@ -13,8 +13,6 @@ global $wp_query; ?>
 		<section id="primary">
 			<div id="content" style='width:800px;' role="main">
 
-			<?php if ( have_posts() ) : ?>
-
 				<header class="page-header">
 					<h1 class="page-title"><?php
 						printf( __( 'The State of %s', 'twentyeleven' ), '<span id="state-title">' . single_cat_title( '', false ) . '</span>' );
@@ -84,24 +82,15 @@ global $wp_query; ?>
 <?php } $i++; ?>
 
 				<?php endwhile; ?>
+				<?php else: ?>
+					<div class="entry-content">
+						<p style='text-align: center;'>Welcome to the Frontier! We don't have any stories from <?php single_cat_title(); ?> yet. If you have a tale to tell, please let us know. <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Return to the Home Page</a></p>
+					</div><!-- .entry-content -->
+				
 				<?php endif; ?>
 </div>
 				<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
 
 			</div><!-- #content -->
 		</section><!-- #primary -->
